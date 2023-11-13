00856f240780414aabd597df250ef63d3e926c02
Martina Ngwuta <martinangwuta@gmail.com>
2023-11-13 23:08:25 +0100

    new changes

--- images/script.js
+++ images/script.js
@@ -8,11 +8,17 @@
   }
 }
 */
-function myFunction() {
+/*function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "nav-menu") {
     x.className += " responsive";
   } else {
     x.className = "nav-menu";
   }
+}
+
+var menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px"
+function togglemenu(){
+  if ()
 }

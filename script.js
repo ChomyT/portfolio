16cbce7cdfbb8916599fd2a9d6c244e3e29d19a8
nielvid <nielvidservices@gmail.com>
2023-11-20 13:08:29 +0100

    code improvements

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 var hamburger = document.getElementById('hamburger')
-console.log(hamburger)
 const mobileMenu = document.getElementById('mobile')
 hamburger.addEventListener('click', () => {
   if(mobileMenu.style.display == 'block'){
@@ -11,29 +10,5 @@ hamburger.addEventListener('click', () => {
   }
   
 })
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-/*function myFunction() {
-  var x = document.getElementsByClass("nav-menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-*/
-/*function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "nav-menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-menu";
-  }
-}
 
-var menuList = document.getElementById("menuList");
-menuList.style.maxHeight = "0px"
-function togglemenu(){
-  if ()
-}
-*/
 

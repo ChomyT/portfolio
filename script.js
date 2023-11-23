76c58a0242df52e2a48c8df6b3f6275c436fb792
nielvid <nielvidservices@gmail.com>
2023-11-23 11:54:16 +0100

    added scroll to top icon

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var hamburger = document.getElementById('hamburger')
 const mobileMenu = document.getElementById('mobile')
+const topArrow = document.getElementById('top-arrow')
 hamburger.addEventListener('click', () => {
   if(mobileMenu.style.display == 'block'){
     mobileMenu.style.display = 'none'
@@ -9,6 +10,13 @@ hamburger.addEventListener('click', () => {
     mobileMenu.style.display = 'block'
   }
   
+})
+
+topArrow.addEventListener('click', () => {
+  window.scrollTo({
+    top:0,
+    behavior:"smooth"
+  })
 })
 
 
